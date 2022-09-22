5f83adadde1d1cf37e3eb39c1f569ffcb7b83e15
rtexelm <texelist@gmail.com>
2022-09-22 19:39:40 -0400

    Fulfilled the specs

--- recursion_exercise/lib/problems.rb
+++ recursion_exercise/lib/problems.rb
@@ -12,7 +12,9 @@
 # pow(3, 4) # => 81
 # pow(4, 3) # => 64
 def pow(base, exponent)
-
+    return 1 if exponent == 0
+    
+    base * pow(base, exponent - 1)
 end
 
 
@@ -35,7 +37,12 @@ end
 # lucas_number(5)   # =>    11
 # lucas_number(9)   # =>    76
 def lucas_number(n)
+    
+    return 2 if n == 0
+    return 1 if n == 1
 
+    # n * (lucas_number(n - 1) + lucas_number(n - 2))
+    (lucas_number(n - 1) + lucas_number(n - 2))
 end
 
 
@@ -51,7 +58,10 @@ end
 # sum_array([5, 2])         # => 7
 # sum_array([4, 10, -1, 2]) # => 15
 def sum_array(array)
-
+    return 0 if array.empty?
+    # array.pop + sum_array(array)
+    array.shift + sum_array(array)
+    # array[0] + sum_array(array[1..-1])
 end
 
 
@@ -67,7 +77,9 @@ end
 # reverse_string("internet")    # => "tenretni"
 # reverse_string("friends")     # => "sdneirf"
 def reverse_string(str)
-
+    return "" if str.empty?
+    str = str[-1] + reverse_string(str[0...-1])
+    str
 end
 
 
@@ -100,5 +112,9 @@ end
 #     2-dimensional array: [['some data']]
 #     3-dimensional array: [[['some data']]]
 def flatten(data)
-
+    return [data] if !data.instance_of? Array
+    flattened = []
+    # data.each {|sub| flattened += flatten(data)} # Forgot the loop variable
+    data.each {|sub| flattened += flatten(sub)}
+    flattened
 end
